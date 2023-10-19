80fb2fe50fa973528f43002a64a78c6d6619ea43
彭子隽 <2200012909@stu.pku.edu.cn>
2023-10-20 00:28:30 +0800

    Make a framework using maven and springboot

---
+++ pku_plant_map/src/main/java/com/group7/pku_plant_map/PkuPlantMapApplication.java
@@ -0,0 +1,13 @@
+package com.group7.pku_plant_map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PkuPlantMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PkuPlantMapApplication.class, args);
+	}
+
+}
---
+++ pku_plant_map/src/test/java/com/group7/pku_plant_map/PkuPlantMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group7.pku_plant_map;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PkuPlantMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
